030c4d8ed59a3a14e6ff23b3c4ba61c13e8ab701
adriannaziel <adrianna.ziel96@gmail.com>
2017-11-12 23:03:56 +0100

    zad4

--- JS/zad4.js
+++ JS/zad4.js
@@ -7,3 +7,26 @@ function showCoords(event) {
     var coords2 = "ekran - X: " + sX + ", Y : " + sY;
     document.getElementById("coords").innerHTML = coords1 + "<br>" + coords2;
 }
+
+
+function isKeyPressed(event) {
+    if (event.altKey) {
+        document.body.setAttribute( "style", 
+      "background-color: blue" ); 
+    } 
+    if (event.shiftKey) {
+        document.body.setAttribute( "style", 
+      "background-color: green" ); 
+    }if (event.ctrlKey) {
+        document.body.setAttribute( "style", 
+      "background-color: purple" ); 
+    }
+ }
+
+ function visitme(){
+ 	document.getElementById("id1").innerHTML = "Wróć do mnie!";
+ }
+
+  function clickme(){
+ 	document.getElementById("id1").innerHTML = "Zobacz mnie!";
+ }
